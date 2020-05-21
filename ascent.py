6e0a2ec1f2fff14afd48009c73ac5db3fab889fb
Lennart Wilde <lennart.wilde@rwth-aachen.de>
2020-05-21 18:08:08 +0200

    Added Config and AscentController
    Signed-off-by: Lennart Wilde <lennart.wilde@rwth-aachen.de>

---
+++ ascent.py
@@ -0,0 +1,10 @@
+import krpc
+import config
+
+class AscendController:
+
+    vessel = None
+
+
+    def __init__(self, _vessel):
+        vessel = _vessel
---
+++ config.py
@@ -0,0 +1,2 @@
+Client_Name = "SpaceX Control"
+remote_ip = "192.168.178.96"
